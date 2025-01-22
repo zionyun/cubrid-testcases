5d65d080b06dcbef2783b0e35edcd891fc9a49cb
zionyun <seeon0001@naver.com>
2025-01-23 08:50:39 +0900

    add comments

--- sql/_05_plcsql/_01_testspec/_03_statement/_13_loop/_03_for_iter/cases/03_13_03-07_normal_space_handling_between_dots.sql
+++ sql/_05_plcsql/_01_testspec/_03_statement/_13_loop/_03_for_iter/cases/03_13_03-07_normal_space_handling_between_dots.sql
@@ -4,6 +4,7 @@
 -- A procedure can be successfully created even if there are no spaces around the range operator (..) in a FOR loop.
 
 --case1: No spaces on both sides
+evaluate 'case1';
 CREATE OR REPLACE PROCEDURE PROC_FOR_TEST IS 
 BEGIN 
     FOR i IN 1..5 LOOP 
@@ -13,6 +14,7 @@ END;
 CALL PROC_FOR_TEST();
 
 --case2: No space on the left side
+evaluate 'case2';
 CREATE OR REPLACE PROCEDURE PROC_FOR_TEST IS
 BEGIN
     FOR i IN 1.. 5 LOOP
@@ -22,6 +24,7 @@ END;
 CALL PROC_FOR_TEST();
 
 --case3: No space on the right side
+evaluate 'case3';
 CREATE OR REPLACE PROCEDURE PROC_FOR_TEST IS
 BEGIN
     FOR i IN 1 ..5 LOOP
@@ -30,16 +33,9 @@ BEGIN
 END;
 CALL PROC_FOR_TEST();
 
---case4:
-CREATE OR REPLACE PROCEDURE PROC_FOR_TEST IS
-BEGIN
-    FOR i IN 1...5 LOOP
-        DBMS_OUTPUT.PUT_LINE('Number: ' || i);
-    END LOOP;
-END;
-CALL PROC_FOR_TEST();
-
---case5:
+--case4: Incorrect usage of the range operator with three dots ('....')
+evaluate 'case4';
+DROP PROCEDURE PROC_FOR_TEST;
 CREATE OR REPLACE PROCEDURE PROC_FOR_TEST IS
 BEGIN
     FOR i IN 1....5 LOOP
@@ -48,6 +44,16 @@ BEGIN
 END;
 CALL PROC_FOR_TEST();
 
+--case5: Incorrect usage of the range operator with four dots ('...')
+evaluate 'case5';
+CREATE OR REPLACE PROCEDURE PROC_FOR_TEST IS
+BEGIN
+    FOR i IN 1...5 LOOP
+        DBMS_OUTPUT.PUT_LINE('Number: ' || i);
+    END LOOP;
+END;
+CALL PROC_FOR_TEST();
 DROP PROCEDURE PROC_FOR_TEST;
+
 
 --+ server-message off
